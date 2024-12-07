e259cc9283881352cf2ca8a92eea334afd50949b
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-07 21:20:32 +0600

    added update router

--- urban-frontend/src/pages/Collection.jsx
+++ urban-frontend/src/pages/Collection.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Collection = () => {
     return (
         <div>
-            this is collection page
+            this is collection page 
         </div>
     );
 };
--- urban-frontend/src/pages/Contact.jsx
+++ urban-frontend/src/pages/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Contact = () => {
     return (
         <div>
-            this is contact page
+            this is contact page 
         </div>
     );
 };
