a962eaf973be437a8a8096e35cd08b73ea5b78da
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-07 23:07:58 +0600

    added active nav

--- urban-frontend/src/components/Navbar.jsx
+++ urban-frontend/src/components/Navbar.jsx
@@ -2,28 +2,29 @@ import { NavLink } from "react-router-dom";
 import { assets } from "../assets/assets";
 const Navbar = () => {
   return (
-    <div className="flex items-center justify-between py-5 font-medium">
-      <img src={assets.logo} className="w-36" alt="" />
+      <div className="flex items-center justify-between py-5 font-medium">
+          <h1 className="text-2xl ">URBAN<span className="text-[#c586a5]  text-2xl">.</span></h1>
+      {/* <img src={assets.logo} className="w-36" alt="" /> */}
 
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
         <NavLink to="/" className="flex flex-col items-center gap-1">
           <p>Home</p>
-          <hr className="w-2/4  border-none h-[1.5px] bg-gray-700" />{" "}
+          <hr className="w-2/4  border-none h-[1.5px] bg-gray-700 hidden" />{" "}
         </NavLink>
 
         <NavLink to="/collection" className="flex flex-col items-center gap-1">
           <p>COLLECTION</p>
-          <hr className="w-2/4  border-none h-[1.5px] bg-gray-700" />{" "}
+          <hr className="w-2/4  border-none h-[1.5px] bg-gray-700 hidden" />{" "}
         </NavLink>
 
         <NavLink to="/about" className="flex flex-col items-center gap-1">
           <p>Home</p>
-          <hr className="w-2/4  border-none h-[1.5px] bg-gray-700" />{" "}
+          <hr className="w-2/4  border-none h-[1.5px] bg-gray-700 hidden" />{" "}
         </NavLink>
 
         <NavLink to="/contact" className="flex flex-col items-center gap-1">
           <p>Home</p>
-          <hr className="w-2/4  border-none h-[1.5px] bg-gray-700" />{" "}
+          <hr className="w-2/4  border-none h-[1.5px] bg-gray-700 hidden" />{" "}
         </NavLink>
       </ul>
     </div>
