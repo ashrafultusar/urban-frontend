28690f60fd6cb792f2c379de18cf1957b8bd6723
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-11 17:33:01 +0600

    added banner image

--- urban-frontend/src/components/Hero.jsx
+++ urban-frontend/src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { assets } from "../assets/assets";
 
 const Hero = () => {
   return (
@@ -18,7 +19,9 @@ const Hero = () => {
         </div>
       </div>
 
-      {/* hero right side */}
+          {/* hero right side */}
+          <img className="w-full sm:w-1/2" src={assets?.hero_img} alt="" />
+
       <div></div>
     </div>
   );
