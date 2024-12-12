9e4d4d22f25bf6f783cac866a6a24d041ef59db4
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-12 18:31:39 +0600

    added shopeContextprovider

--- urban-frontend/src/context/ShopContex.jsx
+++ urban-frontend/src/context/ShopContex.jsx
@@ -1,11 +1,14 @@
 import { createContext } from "react";
+import { products } from '../assets/assets'
 
 export const ShopContext = createContext()
 
 const shopContextProvider = (props) => {
-    
+    const currency = '$';
+    const delivery_fee = 10;
+
     const value = {
-        
+        products,currency,delivery_fee
     }
     
     return (
