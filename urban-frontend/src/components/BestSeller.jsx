f93a98aedcb58a730c78e8b877bf79ebc53ddc68
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-16 01:46:12 +0600

    added update

--- urban-frontend/src/components/BestSeller.jsx
+++ urban-frontend/src/components/BestSeller.jsx
@@ -17,9 +17,7 @@ console.log(bestSeller);
       <div className="text-center text-3xl py-8">
         <Title text1={"BEST"} text2={"SELLERS"}></Title>
         <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
-          {" "}
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus,
-          exercitationem?
+
         </p>
       </div>
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
