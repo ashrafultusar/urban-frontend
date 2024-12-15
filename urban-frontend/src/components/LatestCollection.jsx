6454e3a2f200256ae74002df8b318b9c2ce0173a
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-16 00:57:10 +0600

    added update

--- urban-frontend/src/components/LatestCollection.jsx
+++ urban-frontend/src/components/LatestCollection.jsx
@@ -1,18 +1,16 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 
-import { ShopContext } from './../context/ShopContex';
-
+import { ShopContext } from "./../context/ShopContex";
+import Title from "./Title";
 
 const LatestCollection = () => {
+  const { products } = useContext(ShopContext);
 
-    const {products}=useContext(ShopContext)
-console.log(products);
-
-    return (
-        <div>
-            
-        </div>
-    );
+    return <div className="my-10">
+        <div className="text-center py-8 text-3xl">
+            <Title text1={'LATEST'} text2={'COLLECTION'}></Title>
+      </div>
+  </div>;
 };
 
 export default LatestCollection;
