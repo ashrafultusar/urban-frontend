52b69d713b76cb9b1a51db120099c86faafe482d
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-16 01:09:54 +0600

    added item on product item page

--- urban-frontend/src/components/ProductItem.jsx
+++ urban-frontend/src/components/ProductItem.jsx
@@ -1,18 +1,23 @@
-import React, { useContext } from 'react';
-import { ShopContext } from '../context/ShopContex';
-import { Link } from 'react-router-dom';
+import React, { useContext } from "react";
+import { ShopContext } from "../context/ShopContex";
+import { Link } from "react-router-dom";
 
 const ProductItem = ({ id, image, name, price }) => {
-    
-    const { currency } = useContext(ShopContext);
+  const { currency } = useContext(ShopContext);
 
-    return (
-        <Link to={`/product/${id}`} className='text-gray-700 cursor-pointer'>
-            <div className=''>
-
-            </div>
-        </Link>
-    );
+  return (
+    <Link to={`/product/${id}`} className="text-gray-700 cursor-pointer">
+      <div className="overflow-hidden">
+        <img
+          className="hover:scale-110 transition ease-in-out"
+          src={image[0]}
+          alt=""
+        />
+          </div>
+          <p className="pt-3 pb-1 text-sm">{name}</p>
+          <p className="text-sm font-medium">{currency}{ price}</p>
+    </Link>
+  );
 };
 
 export default ProductItem;
