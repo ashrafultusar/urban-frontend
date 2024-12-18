51dc6d4d47b1b1e455be17f287050c9d732611af
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-18 23:07:06 +0600

    added uptade newsletter box

--- urban-frontend/src/components/NewsLetterBox.jsx
+++ urban-frontend/src/components/NewsLetterBox.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const NewsLetterBox = () => {
     return (
-        <div>
-            
+        <div className='text-center'>
+            <p>Subscribe now & get 20% off</p>
         </div>
     );
 };
