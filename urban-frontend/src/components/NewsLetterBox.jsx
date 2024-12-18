80e45280b4ba0bf723e580fc555a1b9804583d0a
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-18 23:23:27 +0600

    added update

--- urban-frontend/src/components/NewsLetterBox.jsx
+++ urban-frontend/src/components/NewsLetterBox.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const NewsLetterBox = () => {
     return (
         <div className='text-center'>
-            <p>Subscribe now & get 20% off</p>
+            <p className='text-2xl font-medium text-gray-800'>Subscribe now & get 20% off</p>
+            <p className='text-gray-400 mt-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            
+            <form>
+<input type="email" placeholder='Enter your email'/>
+            </form>
         </div>
     );
 };
