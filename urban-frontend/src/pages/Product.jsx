87905cb4af977df52e63b444ca23f23a4a4f075e
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-25 23:29:21 +0600

    added review and description code

--- urban-frontend/src/pages/Product.jsx
+++ urban-frontend/src/pages/Product.jsx
@@ -5,10 +5,10 @@ import { assets } from "../assets/assets";
 
 const Product = () => {
   const { productId } = useParams();
-  const { products ,currency} = useContext(ShopContext);
+  const { products, currency } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
-    const [image, setImage] = useState("");
-    const [size,setSize]=useState('')
+  const [image, setImage] = useState("");
+  const [size, setSize] = useState("");
 
   const fetchProductData = async () => {
     products.map((item) => {
@@ -55,23 +55,47 @@ const Product = () => {
             <img className="w-3" src={assets.star_dull_icon} alt="" />
             <p className="pl-2">(122)</p>
           </div>
-                  <p className="mt-5 text-3xl font-medium">{currency}{productData.price}</p>
-                  <p className="mt-5 text-gray-500 md:w-4/5">{productData.description}</p>
-                  <div className="flex flex-col gap-4 my-8">
-                      <p>Select Size</p>
-                      <div className="flex gap-2">
-                          {productData.sizes.map((item, index) => (<button onClick={()=>setSize(item)} key={index} className={`border py-2 px-4 bg-gray-100 ${item===size?'border-orange-500':''}`}>{ item}</button>))}
-                      </div>
-                  </div>
-                  <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">ADD TO CART</button>
-                  <hr className="mt-8 sm:w-4/5"/>
-                  <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
-                      <p>100% Original Product.</p>
-                      <p>Cash on Delivery is available on this product.</p>
-                      <p>Easy return and exchange policy within 7 days.</p>
-</div>
+          <p className="mt-5 text-3xl font-medium">
+            {currency}
+            {productData.price}
+          </p>
+          <p className="mt-5 text-gray-500 md:w-4/5">
+            {productData.description}
+          </p>
+          <div className="flex flex-col gap-4 my-8">
+            <p>Select Size</p>
+            <div className="flex gap-2">
+              {productData.sizes.map((item, index) => (
+                <button
+                  onClick={() => setSize(item)}
+                  key={index}
+                  className={`border py-2 px-4 bg-gray-100 ${
+                    item === size ? "border-orange-500" : ""
+                  }`}
+                >
+                  {item}
+                </button>
+              ))}
+            </div>
+          </div>
+          <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">
+            ADD TO CART
+          </button>
+          <hr className="mt-8 sm:w-4/5" />
+          <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
+            <p>100% Original Product.</p>
+            <p>Cash on Delivery is available on this product.</p>
+            <p>Easy return and exchange policy within 7 days.</p>
+          </div>
+        </div>
+          </div>
+          {/* description and review section */}
+          <div className="mt-20">
+              <div className="flex">
+                  <b className="border px-5 py-3 text-sm">Description</b>
+                  <p className="border px-5 py-3 text-sm">Reviews (122)</p>
               </div>
-      </div>
+</div>
     </div>
   ) : (
     <div className="opacity-0"></div>
