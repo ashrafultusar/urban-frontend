b1fd7d61e86c48d494bca475635be22ad4f49441
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-25 20:34:05 +0600

    added update

--- urban-frontend/src/pages/Product.jsx
+++ urban-frontend/src/pages/Product.jsx
@@ -6,13 +6,13 @@ const Product = () => {
   const { productId } = useParams();
   const { products } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
-const [image,setImage]=useState('')
-    
+  const [image, setImage] = useState("");
+
   const fetchProductData = async () => {
     products.map((item) => {
       if (item._id === productId) {
-          setProductData(item);
-          setImage(item.image[0])
+        setProductData(item);
+        setImage(item.image[0]);
         return null;
       }
     });
@@ -22,11 +22,20 @@ const [image,setImage]=useState('')
     fetchProductData();
   }, [productId, products]);
 
-    return productData? (
-        <div>
-      
+  return productData ? (
+      <div className="border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100">
+          <div className="flex gap-12 sm:gap-12 flex-col sm:flex-row">
+              {/* product image */}
+              <div className="flex-1 flex flex-col-reverse gap-3 sm:flex-row">
+                  <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full">
+                      
+                  </div>
+</div>
+          </div>
     </div>
-    ) : <div className="opacity-0"></div>
+  ) : (
+    <div className="opacity-0"></div>
+  );
 };
 
 export default Product;
