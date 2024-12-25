a1c824be83bb73ca61e03113b9d8bf8712a46a2c
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-25 16:52:12 +0600

    added update

--- urban-frontend/src/pages/Collection.jsx
+++ urban-frontend/src/pages/Collection.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react";
 import { ShopContext } from "../context/ShopContex";
 import { assets } from "../assets/assets";
+import Title from "../components/Title";
 
 const Collection = () => {
   const { products } = useContext(ShopContext);
@@ -59,7 +60,20 @@ const Collection = () => {
             </p>
           </div>
         </div>
-      </div>
+          </div>
+          
+          {/* right side code */}
+<div className="flex-1">
+              <div className="flex justify-between text-base sm:text-2xl mb-4">
+                  <Title text1={"ALL"} text2={'COLLECTION'}></Title>
+                  {/* product sort */}
+                  <select className="border-2 border-gray-300 text-sm px-2">
+                      <option value="relavent">Sort by: Relavent</option>
+                      <option value="low-high">Sort by: low-high</option>
+                      <option value="high-low">Sort by: high-low</option>
+                  </select>
+              </div>
+          </div>
     </div>
   );
 };
