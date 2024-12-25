f56dd3550348a056791ecc2a9827d9fd321b92b1
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-25 20:28:33 +0600

    added update

--- urban-frontend/src/pages/Product.jsx
+++ urban-frontend/src/pages/Product.jsx
@@ -1,25 +1,32 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { ShopContext } from '../context/ShopContex';
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { ShopContext } from "../context/ShopContex";
 
 const Product = () => {
-    const { productId } = useParams();
-    const { products } = useContext(ShopContext)
-    const [productData, setProductData] = useState(false)
+  const { productId } = useParams();
+  const { products } = useContext(ShopContext);
+  const [productData, setProductData] = useState(false);
+const [image,setImage]=useState('')
     
-    const fetchProductData = async () => {
-        
-    }
+  const fetchProductData = async () => {
+    products.map((item) => {
+      if (item._id === productId) {
+          setProductData(item);
+          setImage(item.image[0])
+        return null;
+      }
+    });
+  };
 
-    useEffect(() => {
-        fetchProductData();
-},[productId,products])
+  useEffect(() => {
+    fetchProductData();
+  }, [productId, products]);
 
-    return (
+    return productData? (
         <div>
-         
-        </div>
-    );
+      
+    </div>
+    ) : <div className="opacity-0"></div>
 };
 
 export default Product;
