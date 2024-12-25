44120c7ff4da3028b14c5d30dd0151920f1fcf27
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-26 00:45:25 +0600

    added update

--- urban-frontend/src/context/ShopContex.jsx
+++ urban-frontend/src/context/ShopContex.jsx
@@ -35,6 +35,21 @@ const ShopContextProvider = (props) => {
     console.log(cartItems);
   }, [cartItems]);
 
+    const getCartCount = () => {
+        let totalCount = 0;
+        for (const items in cartItems) {
+            for (const item in cartItems[items]) {
+                try {
+                    if (cartItems[items][item]>0) {
+                        totalCount+=cartItems[items][item]
+                    }
+                } catch(error ){
+                    
+                }
+            }
+        }
+    }
+    
   const value = {
     products,
     currency,
