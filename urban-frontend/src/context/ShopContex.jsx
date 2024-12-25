79df9b6a96879a101717bdeecccce26acf369607
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-26 00:27:20 +0600

    added up

--- urban-frontend/src/context/ShopContex.jsx
+++ urban-frontend/src/context/ShopContex.jsx
@@ -12,7 +12,16 @@ const [cartItems,setCartItems]=useState({})
     
     const addToCart = async (itemId,size) => {
         let cartData = structuredClone(cartItems);
-
+if (cartData[itemId]) {
+    if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+    } else {
+        cartData[itemId][size] = 1;
+    }
+} else {
+    cartData[itemId] = {};
+    cartData[itemId][size] = 1;
+}
     }
     
   const value = {
