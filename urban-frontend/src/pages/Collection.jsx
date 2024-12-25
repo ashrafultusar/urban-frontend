ba07484459aa6099f4f4e4011a0c679bdca02e19
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-25 18:25:52 +0600

    added filter function updat

--- urban-frontend/src/pages/Collection.jsx
+++ urban-frontend/src/pages/Collection.jsx
@@ -36,6 +36,9 @@ const [subCategory,setSubCategory]=useState([])
         if (category.length > 0) {
             productsCopy=productsCopy.filter(item=>category.includes(item.category))
         }
+        if (subCategory.length > 0) {
+            productsCopy=productsCopy.filter(item=>subCategory.includes(item.subCategory))
+        }
 
         setFilterProducts(productsCopy)
     }
