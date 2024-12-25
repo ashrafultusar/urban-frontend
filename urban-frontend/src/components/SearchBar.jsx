f50aed3f994832a20bfa5a29ef6905e412b2110f
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-25 18:52:32 +0600

    added update search design

--- urban-frontend/src/components/SearchBar.jsx
+++ urban-frontend/src/components/SearchBar.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { ShopContext } from "../context/ShopContex";
+import { assets } from "../assets/assets";
 
 const SearchBar = () => {
 const {search,setSearch,showSearch,setShowSearch}=useContext(ShopContext)
@@ -7,7 +8,8 @@ const {search,setSearch,showSearch,setShowSearch}=useContext(ShopContext)
     return showSearch?(
         <div className="border-t border-b bg-gray-50 text-center">
             <div className="inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2">
-<input className="flex-1 outline-none bg-inherit text-sm" type="text" placeholder="Search" />
+                <input value={search} onChange={(e) => setSearch(e.target.value)} className="flex-1 outline-none bg-inherit text-sm" type="text" placeholder="Search" />
+                <img src={assets.search_icon} alt="" />
             </div>
         </div>
     ):null
