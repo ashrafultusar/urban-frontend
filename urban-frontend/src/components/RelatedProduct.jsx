8ea81e0eeaaff5d38ba1d74596542f17be5ce842
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-26 00:02:35 +0600

    added update

--- urban-frontend/src/components/RelatedProduct.jsx
+++ urban-frontend/src/components/RelatedProduct.jsx
@@ -1,10 +1,20 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { ShopContext } from '../context/ShopContex';
 
 const RelatedProduct = ({ category, subCategory }) => {
     
     const{products}=useContext(ShopContext)
+const [related,setRelated]=useState([])
 
+    useEffect(() => {
+        
+        if (products.length>0) {
+            let productsCopy = products.slice();
+            
+    }
+
+    }, [products])
+    
     return (
         <div>
             
