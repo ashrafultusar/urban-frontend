f12657c07e220c2a51deb5a61163cadd43fc46e0
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-25 16:33:20 +0600

    added categori filter design update

--- urban-frontend/src/pages/Collection.jsx
+++ urban-frontend/src/pages/Collection.jsx
@@ -18,13 +18,22 @@ const Collection = () => {
           className={`border border-gray-300 pl-5 py-3 mt-6 ${
             showFilter ? "" : "hidden"
           } sm:block`}
-              >
-                  <p className="mb-3 text-sm font-medium">CATEGORIES</p>
-                  <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
-                      <p className="">
-                          
-                      </p>
-                  </div>
+        >
+          <p className="mb-3 text-sm font-medium">CATEGORIES</p>
+          <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+            <p className="flex gap-2">
+              <input type="checkbox" className="w-3" value={"Men"} />
+              Men
+            </p>
+            <p className="flex gap-2">
+              <input type="checkbox" className="w-3" value={"Women"} />
+              Women
+            </p>
+            <p className="flex gap-2">
+              <input type="checkbox" className="w-3" value={"Kids"} />
+              Kids
+            </p>
+          </div>
         </div>
       </div>
     </div>
