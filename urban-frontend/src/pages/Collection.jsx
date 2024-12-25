31b567152d1dc52c4e6e651a66887aba107e7c23
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-25 16:36:10 +0600

    added subcategory design

--- urban-frontend/src/pages/Collection.jsx
+++ urban-frontend/src/pages/Collection.jsx
@@ -35,6 +35,28 @@ const Collection = () => {
             </p>
           </div>
         </div>
+        {/* subcategory filter */}
+        <div
+          className={`border border-gray-300 pl-5 py-3 mt-6 ${
+            showFilter ? "" : "hidden"
+          } sm:block`}
+        >
+          <p className="mb-3 text-sm font-medium">TYPE</p>
+          <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+            <p className="flex gap-2">
+              <input type="checkbox" className="w-3" value={"Topwear"} />
+              Topwear
+            </p>
+            <p className="flex gap-2">
+              <input type="checkbox" className="w-3" value={"Bottomwear"} />
+              Bottomwear
+            </p>
+            <p className="flex gap-2">
+              <input type="checkbox" className="w-3" value={"Winterwear"} />
+              Winterwear
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
