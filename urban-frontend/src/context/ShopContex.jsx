d88f0f17c73780ea3e6122772b4a9f0b34098b70
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-26 00:38:49 +0600

    added update

--- urban-frontend/src/context/ShopContex.jsx
+++ urban-frontend/src/context/ShopContex.jsx
@@ -1,5 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+import { toast } from "react-toastify";
 
 export const ShopContext = createContext();
 
@@ -8,25 +9,31 @@ const ShopContextProvider = (props) => {
   const delivery_fee = 10;
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
-const [cartItems,setCartItems]=useState({})
-    
-    const addToCart = async (itemId,size) => {
-        let cartData = structuredClone(cartItems);
-if (cartData[itemId]) {
-    if (cartData[itemId][size]) {
+  const [cartItems, setCartItems] = useState({});
+
+    const addToCart = async (itemId, size) => {
+      
+        if (!size) {
+            toast.error('Select Product Size')
+            return
+}
+
+    let cartData = structuredClone(cartItems);
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
         cartData[itemId][size] += 1;
-    } else {
+      } else {
         cartData[itemId][size] = 1;
+      }
+    } else {
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
     }
-} else {
-    cartData[itemId] = {};
-    cartData[itemId][size] = 1;
-        }
-        setCartItems(cartData)
-    }
-    useEffect(() => {
-        console.log(cartItems);
-    },[cartItems])
+    setCartItems(cartData);
+  };
+  useEffect(() => {
+    console.log(cartItems);
+  }, [cartItems]);
 
   const value = {
     products,
@@ -35,8 +42,9 @@ if (cartData[itemId]) {
     search,
     setSearch,
     showSearch,
-      setShowSearch,
-    cartItems,addToCart
+    setShowSearch,
+    cartItems,
+    addToCart,
   };
 
   return (
