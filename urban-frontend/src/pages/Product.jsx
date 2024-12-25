1597f05a20e22e45dea5aefc0501b6ee31de4737
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-25 23:19:49 +0600

    added button update

--- urban-frontend/src/pages/Product.jsx
+++ urban-frontend/src/pages/Product.jsx
@@ -7,7 +7,8 @@ const Product = () => {
   const { productId } = useParams();
   const { products ,currency} = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
-  const [image, setImage] = useState("");
+    const [image, setImage] = useState("");
+    const [size,setSize]=useState('')
 
   const fetchProductData = async () => {
     products.map((item) => {
@@ -59,7 +60,7 @@ const Product = () => {
                   <div className="flex flex-col gap-4 my-8">
                       <p>Select Size</p>
                       <div className="flex gap-2">
-                          {productData.sizes.map((item, index) => (<button key={index} className={`border py-2 px-4 bg-gray-100`}>{ item}</button>))}
+                          {productData.sizes.map((item, index) => (<button onClick={()=>setSize(item)} key={index} className={`border py-2 px-4 bg-gray-100 ${item===size?'border-orange-500':''}`}>{ item}</button>))}
                       </div>
                   </div>
               </div>
