d6fe2fb23492d4f369fb5dc1a7e30b74cb4afb8d
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-26 23:38:19 +0600

    added update delevery order page

--- urban-frontend/src/pages/PlaceOrder.jsx
+++ urban-frontend/src/pages/PlaceOrder.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
+import Title from "../components/Title";
 
 const PlaceOrder = () => {
-    return (
-        <div className='flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t'>
-           
-        </div>
-    );
+  return (
+    <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t">
+      {/* left side code */}
+      <div className="flex flex-col gap-4 w-full sm:max-w-[480px]">
+        <div className="text-xl sm:text-2xl my-3">
+          <Title text1={"DELIVERY"} text2={"INFORMATION"}></Title>
+              </div>
+              <div className="flex gap-3">
+                  
+              </div>
+      </div>
+    </div>
+  );
 };
 
 export default PlaceOrder;
