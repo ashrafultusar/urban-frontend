893ae90bbaf4ad155fdd0f548b69348199e499a5
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-27 00:06:01 +0600

    added update dynimic colore on payment button

--- urban-frontend/src/pages/PlaceOrder.jsx
+++ urban-frontend/src/pages/PlaceOrder.jsx
@@ -75,15 +75,15 @@ const PlaceOrder = () => {
           {/* payment method selection */}
           <div className="flex gap-3 flex-col lg:flex-row">
             <div onClick={()=>setMethod('stripe')} className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
-              <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+              <p className={`min-w-3.5 h-3.5 border rounded-full ${method===''?'bg-green-400':''}`}></p>
               <img className="h-5 mx-4" src={assets.stripe_logo} alt="" />
             </div>
             <div onClick={()=>setMethod('razorpay')} className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
-              <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+              <p className={`min-w-3.5 h-3.5 border rounded-full ${method===''?'bg-green-400':''}`}></p>
               <img className="h-5 mx-4" src={assets.razorpay_logo} alt="" />
             </div>
             <div onClick={()=>setMethod('cod')} className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
-              <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+              <p className={`min-w-3.5 h-3.5 border rounded-full ${method===''?'bg-green-400':''}`}></p>
               <p className="text-gray-500 text-sm font-medium mx-4">
                 CASH ON DELIVERY
               </p>
