c55f9949a3e6ae48e4fac9e2314fb3cc5df14ccd
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-26 23:35:24 +0600

    added placeorder

--- urban-frontend/src/pages/PlaceOrder.jsx
+++ urban-frontend/src/pages/PlaceOrder.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const PlaceOrder = () => {
     return (
-        <div>
-            placeorder 
+        <div className='flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t'>
+           
         </div>
     );
 };
