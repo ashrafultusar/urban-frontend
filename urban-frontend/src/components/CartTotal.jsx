c775559ee2aea51f157006e868a66eaac16f8efd
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-26 23:05:52 +0600

    added total cart item update

--- urban-frontend/src/components/CartTotal.jsx
+++ urban-frontend/src/components/CartTotal.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ShopContext } from '../context/ShopContex';
 
 const CartTotal = () => {
+    const { currency, delivery_fee, getCartAmount } = useContext(ShopContext);
+
     return (
         <div>
             
