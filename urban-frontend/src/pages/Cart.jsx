54e2d4b6e0a45cc356980682a3993e18db813a93
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-26 21:35:38 +0600

    added cart item data

--- urban-frontend/src/pages/Cart.jsx
+++ urban-frontend/src/pages/Cart.jsx
@@ -7,7 +7,19 @@ const Cart = () => {
     const [cartData, setCartData] = useState([]);
     
     useEffect(() => {
-        
+        const tempData = [];
+        for (const items in cartItems) {
+            for (const item in cartItems[items]) {
+                if (cartItems[items][item]>0) {
+                    tempData.push({
+                        _id: items,
+                        size: item,
+                        quantity:cartItems[items][item]
+                })
+                }
+           }
+        }
+        console.log(tempData);
     },[cartItems])
 
     return <div>
