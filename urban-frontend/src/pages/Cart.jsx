e2fd3cf020244a4cbf17e80336ed81b64dadc26b
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-26 21:55:34 +0600

    added update

--- urban-frontend/src/pages/Cart.jsx
+++ urban-frontend/src/pages/Cart.jsx
@@ -5,29 +5,48 @@ import Title from "../components/Title";
 const Cart = () => {
   const { products, currency, cartItems } = useContext(ShopContext);
 
-    const [cartData, setCartData] = useState([]);
-    
-    useEffect(() => {
-        const tempData = [];
-        for (const items in cartItems) {
-            for (const item in cartItems[items]) {
-                if (cartItems[items][item]>0) {
-                    tempData.push({
-                        _id: items,
-                        size: item,
-                        quantity:cartItems[items][item]
-                })
-                }
-           }
-        }
-        setCartData(tempData);
-    },[cartItems])
+  const [cartData, setCartData] = useState([]);
 
-    return <div className="border-t pt-14">
-        <div className="text-2xl mb-3">
-            <Title text1={'YOUR'} text2={'CART'}></Title>
+  useEffect(() => {
+    const tempData = [];
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        if (cartItems[items][item] > 0) {
+          tempData.push({
+            _id: items,
+            size: item,
+            quantity: cartItems[items][item],
+          });
+        }
+      }
+    }
+    setCartData(tempData);
+  }, [cartItems]);
+
+  return (
+    <div className="border-t pt-14">
+      <div className="text-2xl mb-3">
+        <Title text1={"YOUR"} text2={"CART"}></Title>
       </div>
-  </div>;
+      <div>
+        {cartData.map((item, index) => {
+          const productData = products.find(
+            (product) => product._id === item._id
+          );
+          return (
+            <div
+              key={index}
+              className="py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4"
+              >
+                  <div>
+                      
+                  </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
