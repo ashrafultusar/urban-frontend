05d34610dbdaecdb5f871df13ee6ed40fe132d26
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-26 23:07:35 +0600

    added cart total page title

--- urban-frontend/src/components/CartTotal.jsx
+++ urban-frontend/src/components/CartTotal.jsx
@@ -1,14 +1,17 @@
-import React, { useContext } from 'react';
-import { ShopContext } from '../context/ShopContex';
+import React, { useContext } from "react";
+import { ShopContext } from "../context/ShopContex";
+import Title from "./Title";
 
 const CartTotal = () => {
-    const { currency, delivery_fee, getCartAmount } = useContext(ShopContext);
+  const { currency, delivery_fee, getCartAmount } = useContext(ShopContext);
 
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="w-full">
+      <div className="text-2xl">
+        <Title text1={"TOTAL"} text2={"CART"}></Title>
+      </div>
+    </div>
+  );
 };
 
 export default CartTotal;
