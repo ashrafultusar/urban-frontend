25dd9203f71d70284811e67e984dd1e5e203e190
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-31 22:13:36 +0600

    added p on about

--- urban-frontend/src/pages/About.jsx
+++ urban-frontend/src/pages/About.jsx
@@ -11,7 +11,10 @@ const About = () => {
           <div className="my-10 flex flex-col md:flex-row gap-16">
               <img className="w-full md:max-w-[450px]" src={assets.about_img} alt="" />
               <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
-                  
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ex assumenda sit repellat, recusandae vel at est qui omnis sequi a autem soluta doloribus</p>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est qui dicta vero similique sed nisi exercitationem culpa doloremque, excepturi ipsam quaerat porro consequatur</p>
+                  <b className="text-gray-800 ">Our Mission</b>
+                  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex laborum distinctio assumenda molestias at accusantium vitae, temporibus facere quos minus odio, suscipit eveniet</p>
               </div>
           </div>
     </div>
