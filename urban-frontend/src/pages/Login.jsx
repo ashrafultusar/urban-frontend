93e9bd6b2f27f5d84fc95d3d2f42b32355d6b2dd
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-31 21:52:31 +0600

    added input fild

--- urban-frontend/src/pages/Login.jsx
+++ urban-frontend/src/pages/Login.jsx
@@ -1,17 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Login = () => {
-
-    const [currentState,setCurrentState]=useState('Sign Up')
-    return (
-        <form className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
-            <div className='inline-flex items-center gap-2 mb-2 mt-10'>
-                <p className='font-prata text-3xl'>{currentState}</p>
-                <hr className='border-none h-[1.5px] w-8 bg-gray-800'/>
-            </div>
-
-        </form>
-    );
+  const [currentState, setCurrentState] = useState("Sign Up");
+  return (
+    <form className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
+      <div className="inline-flex items-center gap-2 mb-2 mt-10">
+        <p className="font-prata text-3xl">{currentState}</p>
+        <hr className="border-none h-[1.5px] w-8 bg-gray-800" />
+          </div>
+          
+          <input required type="text" className="w-full px-3 py-2 border border-gray-800" placeholder="Name"/>
+          <input required type="email" className="w-full px-3 py-2 border border-gray-800" placeholder="Email" />
+          <input required type="password" className="w-full px-3 py-2 border border-gray-800" placeholder="Password"/>
+    </form>
+  );
 };
 
 export default Login;
