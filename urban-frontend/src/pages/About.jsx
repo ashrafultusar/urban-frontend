1f7a130a9063ac43130e5a3f24fe291694a8e206
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-31 22:11:34 +0600

    added update about page

--- urban-frontend/src/pages/About.jsx
+++ urban-frontend/src/pages/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Title from "../components/Title";
+import { assets } from "../assets/assets";
 
 const About = () => {
   return (
@@ -8,7 +9,10 @@ const About = () => {
         <Title text1={"about"} text2={"us"}></Title>
           </div>
           <div className="my-10 flex flex-col md:flex-row gap-16">
-              
+              <img className="w-full md:max-w-[450px]" src={assets.about_img} alt="" />
+              <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+                  
+              </div>
           </div>
     </div>
   );
