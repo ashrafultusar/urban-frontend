d268173c8cd441102e6160aeb4a280ce4a4d4c73
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-31 21:58:29 +0600

    added update login page

--- urban-frontend/src/pages/Login.jsx
+++ urban-frontend/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 const Login = () => {
-  const [currentState, setCurrentState] = useState("Login");
+  const [currentState, setCurrentState] = useState("Sign Up");
   return (
     <form className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
       <div className="inline-flex items-center gap-2 mb-2 mt-10">
@@ -30,7 +30,13 @@ const Login = () => {
         type="password"
         className="w-full px-3 py-2 border border-gray-800"
         placeholder="Password"
-      />
+          />
+          <div className="w-full flex justify-between text-sm mt-[-8px]">
+              <p className="cursor-pointer">Forgot Your Password?</p>
+              {
+                  currentState==="Login"?<p className="cursor-pointer">Create account</p>:<p className="cursor-pointer">Login Here</p>
+              }
+          </div>
     </form>
   );
 };
