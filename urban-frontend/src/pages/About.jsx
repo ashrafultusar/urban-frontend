ddf2cdbe763c7f661f628ab76cbaca57145e7de5
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-31 22:09:09 +0600

    added about page

--- urban-frontend/src/pages/About.jsx
+++ urban-frontend/src/pages/About.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
+import Title from "../components/Title";
 
 const About = () => {
-    return (
-        <div>
-           this is about page  
-        </div>
-    );
+  return (
+    <div>
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={"about"} text2={"us"}></Title>
+          </div>
+          <div className="my-10 flex flex-col md:flex-row gap-16">
+              
+          </div>
+    </div>
+  );
 };
 
 export default About;
