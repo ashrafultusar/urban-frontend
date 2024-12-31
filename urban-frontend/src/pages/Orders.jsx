e92f3afd5a5315e4af35fd49a72c7358ea13fba7
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-31 21:23:01 +0600

    added order page product class

--- urban-frontend/src/pages/Orders.jsx
+++ urban-frontend/src/pages/Orders.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { ShopContext } from "../context/ShopContex";
 import Title from "../components/Title";
+import index from "toastify";
 
 const Orders = () => {
   const { products, currency } = useContext(ShopContext);
@@ -9,7 +10,20 @@ const Orders = () => {
     <div className="border-t pt-16">
           <div className="text-2xl">
               <Title text1={'my'} text2={'order'}></Title>
-      </div>
+     
+              <div>
+                  {
+                      products.slice(1, 4).map((item, index) => (
+                          <div key={index} className="py-4 border-t border-b text-gray-700 flex flex-col md:flex-row md:items-center md:justify-between gap-4">
+                              <div>
+                                  
+                              </div>
+                          </div>
+                      ))
+                  }
+     </div>
+              
+          </div>
     </div>
   );
 };
