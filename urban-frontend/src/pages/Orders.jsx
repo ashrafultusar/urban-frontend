4e08b913cf56dfb3edb2c09e24cf2e4f2b227ac3
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-31 21:26:36 +0600

    update: order page title

--- urban-frontend/src/pages/Orders.jsx
+++ urban-frontend/src/pages/Orders.jsx
@@ -17,6 +17,12 @@ const Orders = () => {
                           <div key={index} className="py-4 border-t border-b text-gray-700 flex flex-col md:flex-row md:items-center md:justify-between gap-4">
                               <div className="flex items-start gap-6 text-sm">
                                   <img className="w-16 sm:w-20" src={item.image[0]} alt="" />
+                                  <div>
+                                      <p className="sm:text-base font-medium">{item.name}</p>
+                                      <div>
+                                          
+                                      </div>
+                                  </div>
                               </div>
                           </div>
                       ))
