53e8b50faeb61076e2788966aa523ac14f769a6c
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-31 21:18:59 +0600

    added order page main title

--- urban-frontend/src/pages/Orders.jsx
+++ urban-frontend/src/pages/Orders.jsx
@@ -1,14 +1,17 @@
-import React, { useContext } from 'react';
-import { ShopContext } from '../context/ShopContex';
+import React, { useContext } from "react";
+import { ShopContext } from "../context/ShopContex";
+import Title from "../components/Title";
 
 const Orders = () => {
-const {products,currency}=useContext(ShopContext)
+  const { products, currency } = useContext(ShopContext);
 
-    return (
-        <div className='border-t pt-16'>
-          
-        </div>
-    );
+  return (
+    <div className="border-t pt-16">
+          <div className="text-2xl">
+              <Title text1={'my'} text2={'order'}></Title>
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
