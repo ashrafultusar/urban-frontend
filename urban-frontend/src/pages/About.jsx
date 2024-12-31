97550439e49af702362fc02b286ceba8613e824e
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-12-31 22:18:10 +0600

    added update

--- urban-frontend/src/pages/About.jsx
+++ urban-frontend/src/pages/About.jsx
@@ -20,6 +20,16 @@ const About = () => {
           <div className="text-4xl py-4">
               <Title text1={'why'} text2={'choose us'}></Title>
           </div>
+          <div className="flex flex-col md:flex-row text-sm mb-20">
+              <div className="border  px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+                  <b>Quality Assurance</b>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              </div>
+              <div className="border  px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+                  <b>Convenience</b>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              </div>
+          </div>
     </div>
   );
 };
