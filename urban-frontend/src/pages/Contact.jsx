1274b70f5dc512e12cfbfbe5a208e11e065b3977
ashrafultusar <ashrafulislamtusar@gmail.com>
2025-01-01 16:39:17 +0600

    added contact page newslatter

--- urban-frontend/src/pages/Contact.jsx
+++ urban-frontend/src/pages/Contact.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Title from "../components/Title";
 import { assets } from "../assets/assets";
+import NewsLetterBox from './../components/NewsLetterBox';
 
 const Contact = () => {
   return (
@@ -30,9 +31,13 @@ const Contact = () => {
           <p className="text-gray-500">
             Learn more about our teams and job openings.
           </p>
-          <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500"> Explore Jobs</button>
+          <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">
+            {" "}
+            Explore Jobs
+          </button>
         </div>
-      </div>
+          </div>
+          <NewsLetterBox></NewsLetterBox>
     </div>
   );
 };
